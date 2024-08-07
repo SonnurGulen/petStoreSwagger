f08cc6cd4da1c2fc860326a07fde70f77c55cc58
Sonnur <sonnurgulen@hotmail.com>
2024-08-07 15:18:41 +0300

    Formatted

--- cypress/e2e/DataHelper/Data.js
+++ cypress/e2e/DataHelper/Data.js
@@ -1,126 +1,126 @@
-class Data{
-    headerPayload() {
-        const headerBody = {
-            "accept": "application/json",
-            "Content-Type": "application/json"
-        }
-        return headerBody;
+class Data {
+  headerPayload() {
+    const headerBody = {
+      "accept": "application/json",
+      "Content-Type": "application/json"
     }
-
-    bodyPayloadDog() {
-        const postData =  {
-            "id": 267646,
-            "category": {
-              "id": 267646,
-              "name": "dog"
-            },
-            "name": "Shiro",
-            "photoUrls": [
-              "string"
-            ],
-            "tags": [
-              {
-                "id": 267646,
-                "name": "maltese"
-              }
-            ],
-            "status": "available"
-          }
-        return postData;
-    }
-    
-    bodyPayloadCat() {
-        const postData ={
-            "id": 9876,
-            "category": {
-              "id": 9876,
-              "name": "cat"
-            },
-            "name": "Zyra",
-            "photoUrls": [
-              "string"
-            ],
-            "tags": [
-              {
-                "id": 9876,
-                "name": "Tekir"
-              }
-            ],
-            "status": "available"
-          }
-        return postData;
-    }
-
-
-    updatePayloadDog(url) {
-        const postData = {
-            "id":267646,
-            "category": {
-              "id":267646,
-              "name": "dog"
-            },
-            "name": "Shiro",
-            "photoUrls": [
-              url
-            ],
-            "tags": [
-              {
-                "id":267646,
-                "name": "maltese"
-              }
-            ],
-            "status": "available"
-          }
-        return postData;
-    }
-
-    updatePayloadCat(sts) {
-        const postData ={
-            "id":9876,
-            "category": {
-              "id":9876,
-              "name": "cat"
-            },
-            "name": "Zyra",
-            "photoUrls": [
-              "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/330px-Cat_November_2010-1a.jpg"
-            ],
-            "tags": [
-              {
-                "id":9876,
-                "name": "Tekir"
-              }
-            ],
-            "status":sts 
-          }
-        return postData;
-    }
-
-    bodyPayloadBird() {
-      const postData ={
-        "id": 342553,
-        "category": {
-          "id": 342553,
-          "name": "string"
-        },
-        "name": "Birdie",
-        "photoUrls": [
-          "string"
-        ],
-        "tags": [
-          {
-            "id": 0,
-            "name": "Maviş"
-          }
-        ],
-        "status": "available"
-      }
-      return postData;
+    return headerBody;
   }
 
-  orderBird(id,quantity) {
-    const postData ={
-      "id":id,
+  bodyPayloadDog() {
+    const postData = {
+      "id": 267646,
+      "category": {
+        "id": 267646,
+        "name": "dog"
+      },
+      "name": "Shiro",
+      "photoUrls": [
+        "string"
+      ],
+      "tags": [
+        {
+          "id": 267646,
+          "name": "maltese"
+        }
+      ],
+      "status": "available"
+    }
+    return postData;
+  }
+
+  bodyPayloadCat() {
+    const postData = {
+      "id": 9876,
+      "category": {
+        "id": 9876,
+        "name": "cat"
+      },
+      "name": "Zyra",
+      "photoUrls": [
+        "string"
+      ],
+      "tags": [
+        {
+          "id": 9876,
+          "name": "Tekir"
+        }
+      ],
+      "status": "available"
+    }
+    return postData;
+  }
+
+
+  updatePayloadDog(url) {
+    const postData = {
+      "id": 267646,
+      "category": {
+        "id": 267646,
+        "name": "dog"
+      },
+      "name": "Shiro",
+      "photoUrls": [
+        url
+      ],
+      "tags": [
+        {
+          "id": 267646,
+          "name": "maltese"
+        }
+      ],
+      "status": "available"
+    }
+    return postData;
+  }
+
+  updatePayloadCat(sts) {
+    const postData = {
+      "id": 9876,
+      "category": {
+        "id": 9876,
+        "name": "cat"
+      },
+      "name": "Zyra",
+      "photoUrls": [
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/330px-Cat_November_2010-1a.jpg"
+      ],
+      "tags": [
+        {
+          "id": 9876,
+          "name": "Tekir"
+        }
+      ],
+      "status": sts
+    }
+    return postData;
+  }
+
+  bodyPayloadBird() {
+    const postData = {
+      "id": 342553,
+      "category": {
+        "id": 342553,
+        "name": "string"
+      },
+      "name": "Birdie",
+      "photoUrls": [
+        "string"
+      ],
+      "tags": [
+        {
+          "id": 0,
+          "name": "Maviş"
+        }
+      ],
+      "status": "available"
+    }
+    return postData;
+  }
+
+  orderBird(id, quantity) {
+    const postData = {
+      "id": id,
       "petId": id,
       "quantity": quantity,
       "shipDate": "2024-08-07T11:22:36.133Z",
@@ -128,19 +128,19 @@ class Data{
       "complete": true
     }
     return postData;
-}
-
-orderBird(id,quantity) {
-  const postData ={
-    "id":id,
-    "petId": id,
-    "quantity": quantity,
-    "shipDate": "2024-08-07T11:22:36.133Z",
-    "status": "placed",
-    "complete": true
   }
-  return postData;
-}
+
+  orderBird(id, quantity) {
+    const postData = {
+      "id": id,
+      "petId": id,
+      "quantity": quantity,
+      "shipDate": "2024-08-07T11:22:36.133Z",
+      "status": "placed",
+      "complete": true
+    }
+    return postData;
+  }
 
 }
 export default Data
--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -2,16 +2,15 @@ import Data from "./DataHelper/Data"
 import Params from "./DataHelper/params"
 
 describe('template spec', () => {
-const data= new Data
-const params = new Params;
-let url = params.url();
+  const data = new Data
+  const params = new Params;
+  let url = params.url();
 
 
   it('Create Dog', () => {
-    
     cy.request({
       method: 'POST',
-      url: url +'/v2/pet',
+      url: url + '/v2/pet',
       body: data.bodyPayloadDog()
     })
       .then((response) => {
@@ -21,130 +20,126 @@ let url = params.url();
 
 
   it('Create Cat', () => {
-    
     cy.request({
       method: 'POST',
-      url: url +'/v2/pet',
+      url: url + '/v2/pet',
       body: data.bodyPayloadCat()
     })
       .then((response) => {
         expect(response.status).to.eq(200)
       })
-
   })
 
   it('Create Bird', () => {
-    
     cy.request({
       method: 'POST',
-      url: url +'/v2/pet',
+      url: url + '/v2/pet',
       body: data.bodyPayloadBird()
     })
       .then((response) => {
         expect(response.status).to.eq(200)
       })
-
   })
 
   it('update dog photo', () => {
-    
     cy.request({
       method: 'PUT',
-      url: url +'/v2/pet',
+      url: url + '/v2/pet',
       body: data.updatePayloadDog("https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTMecBx0CBg19RNOf9nyN6GzFsc0zSAKMoaf2MSSI1tevIoMzdR")
     })
       .then((response) => {
         expect(response.status).to.eq(200)
       })
-
   })
 
   it('update cat as sold', () => {
-  
     cy.request({
       method: 'PUT',
-      url: url +'/v2/pet',
+      url: url + '/v2/pet',
       body: data.updatePayloadCat('sold')
     })
       .then((response) => {
         expect(response.status).to.eq(200)
       })
-
   })
 
   it('Check Cat', () => {
-
-    cy.request('GET', url +`/v2/pet/9876`)
+    cy.request('GET', url + `/v2/pet/9876`)
       .then((response) => {
         expect(response.status).to.eq(200)
       })
   })
 
-  
-  it('Check sold Pets', () => {
 
-    cy.request('GET', url +`/v2/pet/findByStatus?status=sold`)
+  it('Check sold Pets', () => {
+    cy.request('GET', url + `/v2/pet/findByStatus?status=sold`)
       .then((response) => {
         expect(response.status).to.eq(200)
       })
   })
 
   it('Check inventory', () => {
-
-    cy.request('GET', url +`/v2/store/inventory`)
+    cy.request('GET', url + `/v2/store/inventory`)
       .then((response) => {
         expect(response.status).to.eq(200)
       })
   })
 
   it('DeleteDog', () => {
-    cy.request({ 
+    cy.request({
       method: 'DELETE',
-     url: url + `/v2/pet/267646`,
-     headers: data.headerPayload()
- }).then((response) => {
+      url: url + `/v2/pet/267646`,
+      headers: data.headerPayload()
+    })
+      .then((response) => {
         expect(response.status).to.eq(200)
-        
       })
   })
 
 
   it('Check Dog', () => {
-
-    cy.request({ 
+    cy.request({
       method: 'GET',
-     url: url +'/v2/pet/267646',
-     failOnStatusCode:false
- })
+      url: url + '/v2/pet/267646',
+      failOnStatusCode: false
+    })
       .then((response) => {
         expect(response.status).to.eq(404);
         expect(response.body.type).to.eq('error');
-        
       })
   })
 
 
   it('Order Bird', () => {
-
-    cy.request({ 
+    cy.request({
       method: 'POST',
-     url: url +'/v2/store/order',
-     body:data.orderBird('342553',1)
- })
+      url: url + '/v2/store/order',
+      body: data.orderBird('342553', 1)
+    })
       .then((response) => {
-        expect(response.status).to.eq(200);        
+        expect(response.status).to.eq(200);
       })
   })
 
   it('Delete Order Bird', () => {
-
-    cy.request({ 
+    cy.request({
       method: 'DELETE',
-     url:'https://petstore.swagger.io/v2/store/order/342553'
-     
- })
+      url: 'https://petstore.swagger.io/v2/store/order/342553'
+    })
       .then((response) => {
-        expect(response.status).to.eq(200);        
+        expect(response.status).to.eq(200);
       })
   })
+
+  it('Check inventory', () => {
+
+    cy.request({
+      method: 'GET',
+      url: 'https://petstore.swagger.io/v2/store/inventory'
+    })
+      .then((response) => {
+        expect(response.status).to.eq(200);
+      })
+  })
+
 })
